2f3bfd0720299667d7f236863a47078f19573c68
Pasi Sarolahti <pasi.sarolahti@iki.fi>
2022-03-19 14:41:10 +0200

    Initial implementation of statistics collector

---
+++ README.md
@@ -0,0 +1,10 @@
+# plustat - Collector and visualization of A+ statistics
+
+*This file is yet work in progress*
+
+Usage:
+`python collector.py config`
+
+## Dependencies
+
+`pip install seaborn`
---
+++ database.py
@@ -0,0 +1,95 @@
+import sqlite3
+import importlib
+import sys
+from sqlite3 import Error
+
+def create_connection(db_file: str) -> sqlite3.Connection:
+    """ create a database connection to a SQLite database """
+    conn = None
+    try:
+        conn = sqlite3.connect(db_file)
+    except Error as e:
+        print(e)
+
+    return conn
+
+
+def execute_query(connection: sqlite3.Connection, query: str) -> None:
+    cursor = connection.cursor()
+    try:
+        cursor.execute(query)
+        connection.commit()
+        print("Query executed successfully")
+    except Error as e:
+        print(f"The error '{e}' occurred")
+
+
+def execute_read_query(connection: sqlite3.Connection, query: str):
+    cursor = connection.cursor()
+    result = None
+    try:
+        cursor.execute(query)
+        result = cursor.fetchall()
+        return result
+    except Error as e:
+        print(f"The error '{e}' occurred")
+
+
+def create_monthly_table(db: sqlite3.Connection) -> None:
+    query = """
+    CREATE TABLE IF NOT EXISTS monthly(
+        id INTEGER PRIMARY KEY AUTOINCREMENT,
+        year INTEGER NOT NULL,
+        month INTEGER NOT NULL,
+        submission_count INTEGER,
+        submitters INTEGER
+    );
+    """
+    execute_query(db, query)
+
+
+def insert_monthly_entry(
+    db: sqlite3.Connection,
+    year: int,
+    month: int,
+    submission_count: int,
+    submitters: int,
+) -> None:
+    query = f"""
+    INSERT INTO
+        monthly (year, month, submission_count, submitters)
+    VALUES
+        ({year}, {month}, {submission_count}, {submitters});
+    """
+    execute_query(db, query)
+
+
+def list_monthly_entries(db: sqlite3.Connection) -> None:
+    query = """
+    SELECT * from monthly
+    """
+    entries = execute_read_query(db, query)
+    for entry in entries:
+        print(entry)
+
+
+def month_exists(db: sqlite3.Connection, year: int, month: int) -> bool:
+    query = f"""
+    SELECT * from monthly where year = {year} and month = {month};
+    """
+    entries = execute_read_query(db, query)
+    if entries:
+        return True
+    else:
+        return False
+
+if __name__ == '__main__':
+    if len(sys.argv) < 2:
+        print("You need to specify config gile as an argument.")
+        exit(-1)
+
+    settings = importlib.import_module(sys.argv[1])
+    db = create_connection(settings.DB_FILE)
+    create_monthly_table(db)
+    #list_monthly_entries(db)
+
